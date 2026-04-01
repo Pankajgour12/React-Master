371e2ca304399ef006a872d20c96e0095a02c5f8
pankajgour12 <pankajgour5000@gmail.com>
2026-04-01 20:08:26 +0530

    refactor: enhance layout and spacing in UseState component

--- component/src/State/UseState.jsx
+++ component/src/State/UseState.jsx
@@ -2,17 +2,35 @@ import React, { useState } from 'react'
 
 const UseState = () => {
     const [count, setCount] = useState(0)
+
+    const [val , setVal] = useState({
+        name: "Pankaj",
+        isBanned: false
+    })
    
 
   return (
     <div className='min-h-screen w-full
-    flex flex-col justify-center items-center bg-stone-900 gap-2
+    flex flex-col justify-center items-center bg-stone-900 gap-5
     '>
-        <h1 className='text-9xl text-amber-50'>{count}</h1>
+        <div className='flex flex-col justify-center items-center gap-3'>
+            <h1 className='text-5xl text-amber-50'>{count}</h1>
         <button
          className='px-4 py-2 bg-sky-300 rounded-2xl text-sm shadow '
         onMouseEnter={() => setCount(prev=> prev+1)}>Incress Value</button>
 
+        </div>
+
+        <div className='flex flex-col gap-3 justify-center items-center'>
+            <h1 className='text-5xl text-amber-50'> Name : {val.name}</h1>
+            <h1 className='text-2xl text-amber-50'> Is Banned : {val.isBanned.toString()}</h1>
+
+            <button
+            onClick={()=>setVal({...val ,isBanned: !val.isBanned})}
+            className='px-4 py-2 text-center  rounded-2xl shadow bg-amber-200 text-black '> Change </button>
+
+
+        </div>
         
   
 
