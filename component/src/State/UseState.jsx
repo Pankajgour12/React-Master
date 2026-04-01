f5c6e34e9cdcee5ad0c03f90274dc9716f40ff33
pankajgour12 <pankajgour5000@gmail.com>
2026-04-01 21:44:55 +0530

    feat: enhance UseState component with additional functionality and improved layout

--- component/src/State/UseState.jsx
+++ component/src/State/UseState.jsx
@@ -1,55 +1,91 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const UseState = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    const [val , setVal] = useState({
-        name: "Pankaj",
-        isBanned: false
-    })
+  const [val, setVal] = useState({
+    name: "Pankaj",
+    isBanned: false,
+  });
 
-      const [text, setText] = useState("");
+  const [data, setData] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  /*  const [text, setText] = useState("");
 
   const handleChange = (e) => {
     setText(e.target.value);
-  };
-   
-
-  return (
-    <div className='min-h-screen w-full
+  } */ return (
+    <div
+      className="min-h-screen w-full
     flex flex-col justify-center items-center bg-stone-900 gap-5
-    '>
-        <div className='flex flex-col justify-center items-center gap-3'>
-          
-      <h1 className='text-5xl text-amber-50'>{count}</h1>
+    "
+    >
+      <div className="flex flex-col justify-center items-center gap-3">
+        <h1 className="text-5xl text-amber-50">{count}</h1>
         <button
-         className='px-4 py-2 bg-sky-300 rounded-2xl text-sm shadow '
-        onClick={() => setCount(prev=> prev+1)}>Incress Value</button>
+          className="px-4 py-2 bg-sky-300 rounded-2xl text-sm shadow "
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          Incress Value
+        </button>
 
-
-        
         <button
-         className='px-4 py-2 bg-sky-300 rounded-2xl text-sm shadow '
-        onClick={() => setCount(prev=> prev-1)}>decress Value</button>
+          className="px-4 py-2 bg-sky-300 rounded-2xl text-sm shadow "
+          onClick={() => setCount((prev) => prev - 1)}
+        >
+          decress Value
+        </button>
+      </div>
 
+      <div className="flex flex-col gap-3 justify-center items-center">
+        <h1 className="text-5xl text-amber-50"> Name : {val.name}</h1>
+        <h1 className="text-3xl text-amber-50">
+          {" "}
+          Is Banned :{" "}
+          <span
+            className={`text-xl text-amber-50 ${val.isBanned ? "text-red-500" : "text-green-500"}`}
+          >
+            {val.isBanned.toString()}
+          </span>
+        </h1>
 
+        <button
+          onClick={() => setVal({ ...val, isBanned: !val.isBanned })}
+          className={`px-4 py-2 text-center  rounded-2xl shadow ${val.isBanned ? "bg-red-400" : "bg-green-400"} text-black `}
+        >
+          {" "}
+          Change{" "}
+        </button>
+      </div>
 
-        </div>
+      <div className="bg-gray-100 px-2 flex flex-col justify-center items-center gap-3 text-black">
+        {data.map((item, i) => (
+          <h1 key={i} className="text-xl">
+            {item}
+          </h1>
+        ))}
+      </div>
 
-        <div className='flex flex-col gap-3 justify-center items-center'>
-            <h1 className='text-5xl text-amber-50'> Name : {val.name}</h1>
-            <h1 className='text-2xl text-amber-50'> Is Banned : {val.isBanned.toString()}</h1>
+      <button
+        onClick={() =>
+          setData(() => data.filter((i, inx) => inx != data.length - 1))
+        }
+        className="px-2 py-1 bg-green-300 rounded-full text-green-600"
+      >
+        delete items
+      </button>
 
-            <button
-            onClick={()=>setVal({...val ,isBanned: !val.isBanned})}
-            className='px-4 py-2 text-center  rounded-2xl shadow bg-amber-200 text-black '> Change </button>
+      <button
+        onClick={() => setData([...data, `${data.length + 1}`])}
+        className="px-2 py-1 bg-blue-300 rounded-full text-blue-700"
+      >
+        Add Item
+      </button>
 
-
-        </div>
-
-        <div className=" flex flex-col justify-center items-center gap-4 bg-gray-100 text-white">
+      {/* TEXT COUNTER */}
+      {/*  <div className=" flex flex-col justify-center items-center gap-4 bg-gray-100 text-white">
       
-      {/* Input */}
+     
       <input
         type="text"
         placeholder="Type something..."
@@ -58,7 +94,7 @@ const UseState = () => {
         className="px-4 py-2 rounded-lg text-black outline-none"
       />
 
-      {/* Output */}
+      
       <h1 className="text-2xl text-emerald-500 font-bold">
         {text ? text : "Type something..."}
       </h1>
@@ -67,16 +103,9 @@ const UseState = () => {
         Characters: {text.length}
       </p>
 
+    </div> */}
     </div>
-        
-  
+  );
+};
 
-
-            
-
-
-    </div>
-  )
-}
-
-export default UseState
+export default UseState;
