71f080d1d23f02cd2b850f626bb9c6962b5bea2f
pankajgour12 <pankajgour5000@gmail.com>
2026-04-02 20:34:37 +0530

    refactor: update structure and content of React elements in script.js

--- basic/script.js
+++ basic/script.js
@@ -1,13 +1,21 @@
 
-      const parent = React.createElement(
-        "div",
-        { id: "parent" },
-        React.createElement("div", { id: "child" }, 
-        [
-          React.createElement("h1", {}, "hello I'm a React Element in h1"),
-          React.createElement("h2", {}, "hello I'm a React Element in h2"),
+      const parent = React.createElement("div",{id:'parent'},[
+        React.createElement("div",{id:'child'},[
+              React.createElement("h1",{},"This is h1 in child"),
+              React.createElement("h2",{},"This is h2 in child")
+
         ]),
-      );
+         React.createElement("div",{id:'child2'},[
+              React.createElement("h1",{},"This is h1 in child 2"),
+              React.createElement("h2",{},"This is h2 in child 2")
+
+        ])
+]
+
+
+      )
+
+
       console.log(parent);
 
       const root = ReactDOM.createRoot(document.getElementById("root"));
