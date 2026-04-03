ad44ff41feb8136f22a0e4cb70fabca04d00a0c7
pankajgour12 <pankajgour5000@gmail.com>
2026-04-03 12:50:04 +0530

    feat: update Card component to display user information and handle friend addition

--- React/props/src/App.jsx
+++ React/props/src/App.jsx
@@ -2,14 +2,47 @@ import "./App.css";
 import Card from "./props/Card";
 
 function App() {
+  const peopleData = [
+  {
+    name: "Aarav Sharma",
+    profession: "Software Engineer",
+    image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e"
+  },
+  {
+    name: "Meera Verma",
+    profession: "Graphic Designer",
+    image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330"
+  },
+  {
+    name: "Kabir Singh",
+    profession: "Photographer",
+    image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d"
+  },
+  {
+    name: "Ananya Gupta",
+    profession: "Doctor",
+    image: "https://images.unsplash.com/photo-1559839734-2b71ea197ec2"
+  },
+  {
+    name: "Rohan Das",
+    profession: "Content Writer",
+    image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d"
+  }
+];
+
+
+
   return (
     <div>
       <div className="min-h-screen flex justify-center bg-stone-800 items-center flex-col gap-10 text-stone-300">
         <h1 className="text-5xl ">This is props</h1>
 
         <div className="flex gap-3">
-          <Card  text="Download Now" color="bg-blue-300" />
-          <Card text="Know more" color="bg-red-400"  />
+          {peopleData.map((item,index)=>(
+            <Card key={index} name={item.name} profession={item.profession} image={item.image}/>
+          ))}
+          
+
         </div>
       </div>
     </div>
--- React/props/src/props/Card.jsx
+++ React/props/src/props/Card.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
 
-const Card = ({text, color}) => {
-  return (
-    <div>
-        <button className={`px-4 py2 ${color} text-black rounded-md hover:bg-blue-400 `}>
+const Card = ({name,profession,image}) => {
 
-            {text}
-        </button>
+ const alerted = ()=>{
+    return(
+       alert(`${name} is added to your friend list`)
+    )
+    
+
+ }
+
+  return (
+    <div className='w-52 bg-stone-700 rounded-xl overflow-hidden '>
+        <div className='w-full h-40 bg-stone-600 rounded-2xl overflow-hidden'>
+            <img src={image} 
+            className='w-full h-full object-cover'
+            
+             alt="img" />
+        </div>
+        <div className='w-full p-5 flex flex-col justify-between gap-2 items-start'>
+          <h2 className='text-2xl text-shadow-amber-50 text-stone-100'>{name}</h2>
+          <h5 className='text-red-100 text-xl'>{profession}</h5>
+
+          <button 
+          onClick={alerted}
+          type='button'
+          className='px-3  mt-4 py-1 text-sm text-white bg-black hover:bg-blue-500 font-semibold rounded-md'>
+
+          add friend
+          </button>
+
+
+        </div>
+        
     </div>
   )
 }
