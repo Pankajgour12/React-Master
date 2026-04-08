7b441207c3a1aaf78af05eb946a19aa7f5fc9eca
pankajgour12 <pankajgour5000@gmail.com>
2026-04-08 12:30:06 +0530

    feat: update Show component to fetch and display user data from API

--- React/Integreat_Api/src/pages/Show.jsx
+++ React/Integreat_Api/src/pages/Show.jsx
@@ -1,57 +1,69 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Show = () => {
   const [users, setUsers] = useState([])
 
+
+
+
   const getUsers = async () => {
-    const api = await axios.get("https://fakestoreapi.com/products")
+    try {
+      const api = await axios.get("https://api.escuelajs.co/api/v1/users")
     setUsers(api.data)
+    console.log(api.data)
+      
+    } catch (error) {
+      console.log(error)  
+      
+    }
   }
+  
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   return (
     <div className="max-w-5xl mx-auto mt-10">
 
       <button
-        onClick={getUsers}
+        // onClick={getUsers}
         className="mt-6 px-6 py-3 ml-10 bg-orange-500 text-white rounded-lg hover:bg-rose-600 transition"
       >
         Get Products
       </button>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-8">
-        {users.map((user, index) => {
+        {users?.map((user, index) => {
           return (
             <div
               key={index}
-              className="bg-white shadow-md rounded-xl p-4 border hover:shadow-xl transition"
+              className="bg-white flex flex-col items-center justify-center shadow-md rounded-xl p-4 border hover:shadow-xl transition"
             >
              
               <img
-                src={user.image}
-                alt={user.title}
-                className="h-40 w-full object-contain mb-3"
+                src={user.avatar}
+                alt={user.name}
+                className="h-32 w-32 rounded-full object-contain mb-3"
               />
 
               
               <h2 className="text-md font-semibold mb-2">
-                {user.title}
+                {user.name}
               </h2>
 
               
               <p className="text-green-600 font-bold mb-1">
-                ₹ {user.price}
+                 {user.email}
               </p>
 
               
-              <p className="text-sm text-gray-500 mb-2">
-                {user.category}
+              <p className="text-sm text-orange-500 mb-2">
+                {user.role}
               </p>
 
               
-              <p className="text-sm text-gray-600 line-clamp-2">
-                {user.description}
-              </p>
+              
             </div>
           )
         })}
