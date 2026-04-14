d6931ad001232bfafe1a6639cb28b807ea05a912
pankajgour12 <pankajgour5000@gmail.com>
2026-04-14 19:34:03 +0530

    Project setup

---
+++ React/Project -01/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div className='bg-stone-800 min-h-screen w-full text-white flex justify-center items-center'>
+    
+
+    <h1>this is app component</h1>
+
+
+   </div>
+  )
+}
+
+export default App
