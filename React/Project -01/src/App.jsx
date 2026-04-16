bcafbeda2c1df8e4bd18948932a2fc94d74763b4
pankajgour12 <pankajgour5000@gmail.com>
2026-04-16 17:14:53 +0530

    redux start

--- React/Project -01/src/App.jsx
+++ React/Project -01/src/App.jsx
@@ -8,8 +8,7 @@ function App() {
    <div className='bg-stone-800 min-h-screen w-full text-white flex justify-center items-center'>
     
 
-    <h1>this is app component</h1>
-
+        
 
    
    </div>
